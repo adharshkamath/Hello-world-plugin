197b599eef83d6f308f74c5c88eb0d5e4fc598f9
Adharsh Kamath <adharshkamathr@gmail.com>
2021-02-11 20:51:46 +0530

    Hello world plugin added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Hello-world-plugin
+# Hello world plugin
+
+This is a simple Hello world plugin that works with the Static Analyzer in GCC (>=10)
+and can be used while building the Linux Kernel.
+
+Copy the plugin file to `scripts/gcc-plugins`. 
+
+Then make the necessary changes to `scripts/gcc-plugins/Kconfig` and `scripts/Makefile.gcc-plugins` to include thi plugin in the list of plugins.
+
+After this, when you build the kernel, specify the path for the version of GCC with analyzer support (>=10) if it's not 
+the default installation, and include the option `-fanalyzer`.
+
+You should be able to see `Hello, from plugin_init!` and `Hello, from the plugin callback!` for every file compiled.
---
+++ hello_world_plugin.c
@@ -0,0 +1,35 @@
+/* 
+    A simple Hello world plugin that works with 
+    the GCC static analyzer during a Kernel build.
+*/
+
+#include "gcc-common.h"
+
+__visible int plugin_is_GPL_compatible;
+
+static struct plugin_info hello_world_analyzer_plugin_info = {
+  .version  = "20210207",
+  .help   = "A simple Hello-World gcc-analyzer-plugin that runs during a kernel build\n",
+};
+
+static void hello_world_analyzer_callback (void *gcc_data, void *user_data)
+{
+    fprintf(stderr, "Hello, from the plugin callback!\n");
+}
+
+__visible int plugin_init (struct plugin_name_args *plugin_info, struct plugin_gcc_version *version)
+{
+  if (!plugin_default_version_check(version, &gcc_version)) {
+    error(G_("Incompatible GCC/Plugin versions!"));
+    return 1;
+  }
+
+  const char *plugin_name = plugin_info->base_name;
+    fprintf(stderr, "Hello, from plugin_init!\n");
+  register_callback (plugin_name,
+         PLUGIN_ANALYZER_INIT,
+         hello_world_analyzer_callback,
+         NULL);
+
+  return 0;
+}
